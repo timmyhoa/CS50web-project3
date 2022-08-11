e1164887439171f022a98651f18121e532d39cfb
Tim Pham <timmyhoa34@gmail.com>
2022-08-10 23:43:08 -0400

    almost finish compose function

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,5 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
 
+
+document.addEventListener('DOMContentLoaded', function() {
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
@@ -20,7 +21,38 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+  
+  
+  //Send it away
+  document.querySelector('#compose-form').onsubmit = () => {
+    //Get recipients, subjects and body
+    recipients = document.querySelector('#compose-recipients').value;
+    subject = document.querySelector('#compose-subject').value;
+    body = document.querySelector('#compose-body').value;
+
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+        recipients: recipients,
+        subject: subject,
+        body: body,
+      })
+    })
+    .then(response => {
+       if (response.ok){
+        load_mailbox('sent');
+        console.log('success');
+      } else {
+        console.log('error');
+        message = document.createElement('h3');
+        message.innerHTML = body['error'];
+        document.querySelector('#compose-view').append(message);
+      };
+    });
+    return false;
+  }
 }
+
 
 function load_mailbox(mailbox) {
   
